874bde4b9e0ca1af3a46395e4471c4bebddb9a0c
ogzCode <oguzguc44@gmail.com>
2022-09-26 17:56:05 +0300

    /new route eklendi

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from bottle import run, route, template
+from bottle import run, route, template, get
 
 @route("/")
 @route("/home")
 def home_page():
     return template("views/home.tpl", rows=None)
+
+@get("/new")
+def new_item_page():
+    return template("views/new.tpl", display=False)
 
 run(debug=True, reloader=True)
